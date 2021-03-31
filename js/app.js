46d559ffe8726d99073192d0af94cad2c38d6597
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-03-31 15:47:15 +0300

    added functions Q1

--- js/app.js
+++ js/app.js
@@ -7,11 +7,10 @@ console.log('before ' + typeof age);//here we put + and it works
 //here if we enter a text 
 
 age = parseInt(age); /***** WAY 1 */
-//here will return a Nan 
+//here will return a Nan
 //Nan means: not a number
 //age = Number(); /****** WAY 2 */
 //console.log('after ', typeof age);//here we put , and it works also
-
 
 /*************************** Confirm box */
 /*let feeling = confirm('what is your feeling');
@@ -21,137 +20,169 @@ if (feeling) {
 } else {
     console.log('hope you feel well');
 }*/
-
-
 let userName = prompt('what is your name?');
 alert('hello ' + userName);
 
 let counter = 0;
 
 // Q1
-let myAge = prompt('is my age > 25?');
-myAge = myAge.toLowerCase();
+let myAge = '';
+function age() {
+  myAge = prompt('is my age > 25?');
+  myAge = myAge.toLowerCase();
 
-if (myAge === 'n' || myAge === 'no') {
+  if (myAge === 'n' || myAge === 'no') {
     alert('great gessing');
     //console.log('my age is lower than ' + myAge);
 
     counter += 1;
-} else if (myAge === 'y' || myAge === 'yes') {
+  } else if (myAge === 'y' || myAge === 'yes') {
     alert('oh no! I am 23');
-} else {
+  } else {
     alert('please answer with only yes/y or no/n with no spaces');
+  }
 }
+age();
 
 //Q2
+
 let homeLocation = prompt('do I live in zarqa?');
 homeLocation = homeLocation.toLowerCase();
 if (homeLocation === 'y' || homeLocation === 'yes') {
-    //console.log('great, I live in zarqa');
-    alert('great gessing');
-    counter++;
+  //console.log('great, I live in zarqa');
+  alert('great gessing');
+  counter++;
 } else if (homeLocation === 'n' || homeLocation === 'no') {
-    alert('oh no');
+  alert('oh no');
 } else {
-    alert('please answer with only yes/y or no/n with no spaces');
+  alert('please answer with only yes/y or no/n with no spaces');
 }
 
 //Q3
 let turkey = prompt('do I love turkey?');
 turkey = turkey.toLowerCase();
 if (turkey === 'y' || turkey === 'yes') {
-    //console.log('great, I really love turkey');
-    alert('great gessing');
-    counter++;
+  //console.log('great, I really love turkey');
+  alert('great gessing');
+  counter++;
 } else if (turkey === 'n' || turkey === 'no') {
-    alert('oh no');
+  alert('oh no');
 } else {
-    alert('please answer with only yes/y or no/n with no spaces');
+  alert('please answer with only yes/y or no/n with no spaces');
 }
-
 
 //Q4
 let galaxy = prompt('do I love choclate?');
 galaxy = galaxy.toLowerCase();
 if (galaxy === 'y' || galaxy === 'yes') {
-    //console.log('great, I really love galaxy choclate');
-    alert('great gessing');
-    counter++;
+  //console.log('great, I really love galaxy choclate');
+  alert('great gessing');
+  counter++;
 } else if (galaxy === 'n' || galaxy === 'no') {
-    alert('oh no');
+  alert('oh no');
 } else {
-    alert('please answer with only yes/y or no/n with no spaces');
+  alert('please answer with only yes/y or no/n with no spaces');
 }
-
 
 //Q5
 let design = prompt('do I love designing?');
 design = design.toLowerCase();
 if (design === 'y' || design === 'yes') {
-    //console.log('great, actully I am a Humble designer');
-    alert('great gessing');
-    counter++;
+  //console.log('great, actully I am a Humble designer');
+  alert('great gessing');
+  counter++;
 } else if (design === 'n' || design === 'no') {
-    alert('oh no');
+  alert('oh no');
 } else {
-    alert('please answer with only yes/y or no/n with no spaces');
+  alert('please answer with only yes/y or no/n with no spaces');
 }
 
-
 //Q6
-let guessNum = prompt('guess a number between 0 & 10, you have 4 opportunities ');
+let guessNum = prompt(
+  'guess a number between 0 & 10, you have 4 opportunities '
+);
 let Num = 4;
 
 for (let index = 0; index < 4; index++) {
-    if (guessNum < Num) {
-        alert('too low, try a different number');
-        guessNum = prompt('guess a number between 0 & 10, you have 4 opportunities ');
-    } else if (guessNum > Num) {
-        alert('too high, try a different number');
-        guessNum = prompt('guess a number between 0 & 10, you have 4 opportunities ');
-    }
+  if (guessNum < Num) {
+    alert('too low, try a different number');
+    guessNum = prompt(
+      'guess a number between 0 & 10, you have 4 opportunities '
+    );
+  } else if (guessNum > Num) {
+    alert('too high, try a different number');
+    guessNum = prompt(
+      'guess a number between 0 & 10, you have 4 opportunities '
+    );
+  }
 }
-if (guessNum == Num) {
-    alert('excelent guessing :)');
-    //console.log('great, actully I chose the number 4');
+if (guessNum === Num) {
+  alert('excelent guessing :)');
+  //console.log('great, actully I chose the number 4');
 
-    counter++;
+  counter++;
+} else if (guessNum !== Num) {
+  alert('oh no! the number is ' + Num);
 }
-else if (guessNum != Num) {
-    alert('oh no! the number is ' + Num);
-}
-
 
 //Q7
 
 let fruits = ['banana', 'watermelon', 'pineapple', 'mango'];
-let guessFruit = prompt('guess kind of fruits that I like, you have 6 opportunities ');
+let guessFruit = '';
 
-let attempts = 0;
-let i = 0;
-for (; attempts < 5; attempts++) {
-    for (; i < fruits.length; i++) {
-        if (guessFruit === fruits[i]) {
-            break;
-        }
+// let i = 0;
+attempts: for (let attempts = 0; attempts < 6; attempts++) {
+  //   for (; i < fruits.length; i++) {
+  //     if (guessFruit === fruits[i]) {
+  //       break;
+  //     }
+  //   }
+  guessFruit = prompt(
+    'guess kind of fruits that I like, you have 6 opportunities '
+  );
+  for (let j = 0; j < fruits.length; j++) {
+    if (fruits[j] === guessFruit) {
+      alert(
+        'great :) there are the list of fruites I like: banana , watermelon , pineapple , mango :)'
+      );
+      counter++;
+      break attempts;
     }
-    if (i === fruits.length) {
-        guessFruit = prompt('guess kind of fruits that I like, you have 6 opportunities ');
-    } else {
-        alert('great :) there are the list of fruites I like: banana , watermelon , pineapple , mango :)');
-        //console.log('great, actully I like banana , watermelon , pineapple and mango');
-        counter++;
-        break;
-    }
-    if (attempts == 5) {
-        alert('oh no! there are the list of fruites I like: banana , watermelon , pineapple , mango :)');
-    }
+  }
+  if (attempts === 5) {
+    alert('sorry no more tries ' + fruits);
+  } else if (attempts < 6) {
+    alert('this is wrong');
+  }
+
+  //   if (i === fruits.length) {
+  //     // alert('Your guess is wrong');
+  //     guessFruit = prompt(
+  //       'guess kind of fruits that I like, you have 6 opportunities '
+  //     );
+  //   } else {
+  //     alert(
+  //       'great :) there are the list of fruites I like: banana , watermelon , pineapple , mango :)'
+  //     );
+  //     //console.log('great, actully I like banana , watermelon , pineapple and mango');
+  //     counter++;
+  //     break;
+  //   }
+  //   if (attempts == 5) {
+  //     alert(
+  //       'oh no! there are the list of fruites I like: banana , watermelon , pineapple , mango :)'
+  //     );
+  //   }
 }
 
-
-
 if (counter > 3) {
-    alert('very good ' + userName + ' your score is ' + counter + ' of 7');
+  alert('very good ' + userName + ' your score is ' + counter + ' of 7');
 } else {
-    alert('actully you dont know me :) ' + userName + ' your score is ' + counter + ' of 7');
+  alert(
+    'actully you dont know me :) ' +
+      userName +
+      ' your score is ' +
+      counter +
+      ' of 7'
+  );
 }
